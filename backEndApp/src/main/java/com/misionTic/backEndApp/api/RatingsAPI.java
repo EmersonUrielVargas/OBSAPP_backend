7f174ef0f395996c7f94eee940a4c1c77754f39f
Franma17 <60074921+Franma17@users.noreply.github.com>
2021-11-19 15:10:20 -0500

    Update Users and Ratings

--- backEndApp/src/main/java/com/misionTic/backEndApp/api/RatingsAPI.java
+++ backEndApp/src/main/java/com/misionTic/backEndApp/api/RatingsAPI.java
@@ -1,10 +1,30 @@
 package com.misionTic.backEndApp.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.misionTic.backEndApp.dao.RatingsDAO;
+import com.misionTic.backEndApp.model.Ratings;
 
 @RestController
 @RequestMapping("ratings")
 public class RatingsAPI {
+	
+	@Autowired
+	private RatingsDAO ratingsDao;
+	
+	@PostMapping("/saveRating")
+	public void saveRating(@RequestBody Ratings rating) {
+		ratingsDao.save(rating);
+	}
+	
+	@PutMapping("/updateRating")
+	public void updateUser(@RequestBody Ratings rating) {
+		ratingsDao.save(rating);
+	}
 
 }
--- backEndApp/src/main/java/com/misionTic/backEndApp/api/UsersAPI.java
+++ backEndApp/src/main/java/com/misionTic/backEndApp/api/UsersAPI.java
@@ -1,16 +1,17 @@
 package com.misionTic.backEndApp.api;
 
-import java.util.List;
-
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.misionTic.backEndApp.dao.UsersDAO;
 import com.misionTic.backEndApp.model.Users;
-
 
 @RestController
 @RequestMapping("users")
@@ -19,6 +20,28 @@ public class UsersAPI {
 	@Autowired
 	private UsersDAO usersDao;
 	
+	@PostMapping("/saveUser")
+	public void saveClient(@RequestBody Users user) {
+		usersDao.save(user);
+	}
+	
+	@PutMapping("/updateUser")
+	public void updateUser(@RequestBody Users user) {
+		usersDao.save(user);
+	}
+	
+	@GetMapping("/askUser/{email}")
+	public Users findUserByEmail(@PathVariable("email") String email) {
+		return usersDao.findUserByEmail(email);
+	}
+	
+	@DeleteMapping("/deleteUser/{email}")
+	public void deleteUser(@PathVariable("email") String email) {
+		usersDao.deleteUserByEmail(email);
+
+	}
+
+
 
 
 }
